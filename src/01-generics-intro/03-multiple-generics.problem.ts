aa28c204d6ba17792413bdb3483cd20a177c1c68
owlsoverjericho <owlsoverjericho@protonmail.com>
2022-11-07 22:34:26 +0200

    init

--- src/01-generics-intro/03-multiple-generics.problem.ts
+++ src/01-generics-intro/03-multiple-generics.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {};
+const returnBothOfWhatIPassIn = <T, U>(a: T, b: U) => {return {a, b}};
 
 it("Should return a tuple of the arguments you pass", () => {
   const result = returnBothOfWhatIPassIn("a", 1);
